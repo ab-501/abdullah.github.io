8f1c5d2ee75eb6b734644b2affd4579853e4c5c3
abbyb <abdullah.butt.2021@uni.strath.ac.uk>
2025-04-14 18:39:05 +0100

    Completed: - Images Redesign
    TODO:
    - Interactivity
    - Paragraphs

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// fetch("header.html")
-//     .then(response => response.text())
-//     .then(data => {
-//         document.getElementById("header-placeholder").innerHTML = data;
-//     });
-
 async function loadCaps() {
     try {
         const response = await fetch("assets/Caps.csv");
@@ -20,92 +14,36 @@ async function loadCaps() {
     }
 }
 
+async function loadCapImages(search) {
+    search = search.concat(".png");
+    try {
+        const response = await fetch(`assets/capImages/${search}`);
+        return response.blob();
+    } catch (err) {
+        console.error("Error loading Caps:", err);
+    }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     loadCaps().then((caps) => {
         let capContainer = document.getElementById("CapGrid");
         caps.forEach((cap) => {
             let card = document.createElement("div");
             card.classList.add("CapCard");
-            styleCapCard(cap, card);
             card.innerHTML = `
-            <!--TODO: add cap images-->
-            <img class="image" src="assets/github.png" width="200" height="200" alt="test">
-            <div class="content">
-                <h1>${cap.Name}</h1>
-                <h2>${cap.CapType}</h2>
-                <h2>${cap.Material}</h2>
-                <h3>${cap.FrontDesign}</h3>
-                <h3>${cap.Country}</h3>
-                <h3>${cap.BeverageType}</h3>
-                <h3>Total Quantity: ${cap.Quantity}</h3>
-            </div>
-            
-        `;
-            const img = card.querySelector(".image");
-            const content = card.querySelector(".content");
-            img.hidden = false;
-            content.hidden = true;
-            card.addEventListener("mouseover", () => {
-                img.hidden = true;
-                content.hidden = false;
-            })
-            card.addEventListener("mouseout", () => {
-                img.hidden = false;
-                content.hidden = true;
-            })
+                <img class="capImage" src="assets/firstLoad.png" width="100" height="100" alt="capImage">
+            `;
+            const img = card.querySelector(".capImage");
+            loadCapImages(cap.Name).then((blob) => {
+                img.src = URL.createObjectURL(blob);
+            });
+            img.onerror = () => {
+                card.style.display = "none";
+            };
             capContainer.appendChild(card);
         });
     });
 })
-
-
-function styleCapCard(cap, card){
-    if(cap.BeverageType === "Water"){
-        card.style.background = "lightblue";
-    }
-    if(cap.Name === "Irn Bru" ||
-        cap.Name === "Fanta" ||
-        cap.Name === "Orangina" ||
-        cap.Name === "Schweppes" ||
-        cap.Name === "J2O" ||
-        cap.Name === "Miranda"
-    ){
-        card.style.background = "orange";
-    }
-    if(cap.Name.startsWith("Oasis")){
-        card.style.background = "royalblue";
-    }
-    if(cap.Name.startsWith("Pepsi")){
-        card.style.background = "royalblue";
-        card.style.color = "ghostwhite"
-    }
-    if(cap.Name.startsWith("Coca")){
-        card.style.background = "indianred";
-    }
-    if(cap.Name.startsWith("Diet")){
-        card.style.background = "silver";
-    }
-    if(cap.Name.startsWith("Tymbark") ||
-        cap.Name.startsWith("7up") ||
-        cap.Name.startsWith("Sprite")
-
-    ){
-        card.style.background = "seagreen";
-    }
-    if(cap.Name.startsWith("Coke") || cap.Name.endsWith("kola")){
-        card.style.background = "black";
-        card.style.color = "ghostwhite";
-    }
-    if(cap.BeverageType.includes("Lager") ||
-        cap.BeverageType.includes("Beer") ||
-        cap.BeverageType.includes("Prosecco") ||
-        cap.BeverageType.includes("Rum") ||
-        cap.BeverageType.includes("Champagne") ||
-        cap.BeverageType.includes("Wine")
-    ){
-        card.style.background = "sandybrown";
-    }
-}
 
 async function loadCoins() {
     try {
@@ -123,62 +61,34 @@ async function loadCoins() {
     }
 }
 
+async function loadCoinImages(search) {
+    search = search.concat(".png");
+    try {
+        const response = await fetch(`assets/coinImages/${search}`);
+        return await response.blob();
+    } catch (err) {
+        console.error("Error loading Coins:", err);
+    }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     loadCoins().then((coins) => {
         let coinContainer = document.getElementById("CoinGrid");
         coins.forEach((coin) => {
             let card = document.createElement("div");
             card.classList.add("CoinCard");
-            styleCoinCard(coin, card);
-
             card.innerHTML = `
-                <!--TODO: add coin images-->
-                <img class="image" src="assets/github.png" width="200" height="200" alt="test">
-                <div class="content">
-                    <h1>${coin.Denomination} ${coin.Currency}</h1>
-                    <h2 style="max-width: 30ch">${coin.FrontDesign}</h2>
-                    <p style="max-width: 40ch; text-align: center">${coin.BackDesign}</p>
-                    <h3>${coin.Country} ${coin.Year}</h3>
-                    <h3>Total Quantity: ${coin.Quantity}</h3>
-                </div>
-        `;
-            const img = card.querySelector(".image");
-            const content = card.querySelector(".content");
-            img.hidden = false;
-            content.hidden = true;
-            card.addEventListener("mouseover", () => {
-                img.hidden = true;
-                content.hidden = false;
-            })
-            card.addEventListener("mouseout", () => {
-                img.hidden = false;
-                content.hidden = true;
-            })
+                <img class="coinImage" src="assets/firstLoad.png" width="100" height="100" alt="coinImage">
+            `;
+            const img = card.querySelector(".coinImage");
+            loadCoinImages(coin.BackDesign).then((blob) => {
+                img.src = URL.createObjectURL(blob);
+            });
+            img.onerror = () => {
+                card.style.display = "none";
+            };
             coinContainer.appendChild(card);
         });
     });
 })
-
-function styleCoinCard(coin, card){
-    if((coin.Currency === "Pence" || coin.Currency === "Euro") && ["10", "20", "50"].includes(coin.Denomination)){
-        card.style.background = "silver";
-    }
-
-    if((coin.Denomination === "5" || coin.Denomination === "1" || coin.Denomination === "2") && coin.Currency === "Pence"){
-        card.style.background = "sandybrown";
-    }
-
-    if(["1", "2",].includes(coin.Denomination) && coin.Currency === "Pound"){
-        card.style.background = "gold";
-    }
-
-    if(["1", "2",].includes(coin.Denomination) && coin.Currency === "Euro"){
-        card.style.background = "lightgoldenrodyellow";
-    }
-
-    if(coin.Denomination === "TESCO"){
-        card.style.background = "blue";
-        card.style.color = "ghostwhite";
-    }
-}
 
